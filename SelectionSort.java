00f67cbb0785e221e2cba66744a286d09e86b92e
Hezecaiah <44009720+Hezecaiah@users.noreply.github.com>
2018-10-10 21:08:12 -0600

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -8,8 +8,6 @@ public class SelectionSort {
 		int target3[] = {1,2,3,4,5,6,7,8,9,10};
 		int target4[] = {10,9,8,7,6,5,4,3,2,1};
 		int targetBig[] = {65,83,85,60,8,52,72,15,79,80,97,46,79,51,42,66,4,28,35,33,60,31,67,99,41,42,3,12,37,40,53,7,62,53,61,36,53,53,81,8,82,87,85,29,3,93,11,98,20,56,64,79,70,41,84,9,73,91,14,10,69,96,50,37,3,18,35,38,35,83,33,29,6,59,39,50,10,88,52,6,19,50,24,28,64,86,19,59,24,69,15,91,100,90,71,96,49,65,14,24};
-		//Implement some larger arrays ordered descending to demonstrate the quadratic increase
-		//in runtime.
 		
 		SelectionSort demo = new SelectionSort();
 		
